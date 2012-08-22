a909375c86670a60efa7410648105b7a614aa7cd
Jeremy Trufier <jeremy@trufier.com>
2012-08-22 15:42:47 +0200

    Here we aregit status

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-node_modules/.bin/brunch build . && tibuild.py -rnv -i dist,.DS_Store,node_modules -o osx -t bundle -d ./dist/osx/ ./ && rm -Rf dist/Resources dist/osx/Storific\ Translator.app
+node_modules/.bin/brunch build . && tibuild.py -rnv -i dist,.DS_Store,node_modules -o osx -t bundle -d ./dist/osx/ ./ && rm -Rf Resources dist/osx/Storific\ Translator.app
